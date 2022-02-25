7332fc86269b8727e64dc0c602f95ae88fa7d254
sallah dia <salampefal@gmail.com>
2022-02-25 06:24:55 +0000

    ajouter fichier pentagone.py

---
+++ pentagone.py
@@ -0,0 +1,15 @@
+from sys import builtin_module_names
+import turtle
+a= turtle.Turtle()
+a.left(90)
+i=1
+while i<=5:
+   a.forward(100)
+   a.left(90)
+   a.forward(100)
+   a.left(90)
+   color="blue" 
+   i+=i
+ 
+
+    
